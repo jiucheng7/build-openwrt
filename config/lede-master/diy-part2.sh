e5d606550879b032df0384c42ebce69a2ffb216b
jiucheng7 <77590781@qq.com>
2025-03-18 20:28:30 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -1,53 +1,154 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
-# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
-#========================================================================================================================
+#=================================================
+# MZwrt script
+#=================================================   
 
-# ------------------------------- Main source started -------------------------------
-#
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
-# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+##配置IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+##更改主机名
+sed -i "s/hostname='.*'/hostname='MzWrt'/g" package/base-files/files/bin/config_generate
 
-# Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
-echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
+#删除默认后台密码
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+##加入作者信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='MzWrt - '/g"  package/lean/default-settings/files/zzz-default-settings
+echo -e "\e[38;5;21m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m
+\e[38;5;81m\    \e[38;5;222m____                 _       __     __  / \e[0m
+\e[38;5;81m\   \e[38;5;190m/ __ \____  ___  ____| |     / /____/ /_ / \e[0m
+\e[38;5;81m\  \e[38;5;45m/ / / / __ \/ _ \/ __ \ | /| / / ___/ __/ / \e[0m
+\e[38;5;81m\ \e[38;5;68m/ /_/ / /_/ /  __/ / / / |/ |/ / /  / /_   / \e[0m
+\e[38;5;81m\ \e[38;5;110m\____/ .___/\___/_/ /_/|__/|__/_/   \__/   / \e[0m
+\e[38;5;81m\     \e[38;5;214m/_/                                    / \e[0m
+\e[38;5;81m\  \e[38;5;33mM Z W r t  By  \e[38;5;82mhttps://github.com/mzwrt\e[0m   / \e[0m
+\e[38;5;21m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m" > package/base-files/files/etc/banner
 
-# Replace the default software source
-# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
-#
-# ------------------------------- Main source ends -------------------------------
+# 开启 irqbalance
+cat <<EOL >> package/base-files/files/etc/uci-defaults/99-irqbalance-settings
+#!/bin/sh
+# 开启 irqbalance
+# 修改 option enabled '0' 为 option enabled '1'
+sed -i "s/option enabled '0'/option enabled '1'/g" /etc/config/irqbalance
 
-# ------------------------------- Other started -------------------------------
-#
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# 取消 option interval '10' 前面的 # 号
+sed -i "s/#option interval '10'/option interval '10'/g" /etc/config/irqbalance
 
-# Fix runc version error
-# rm -rf ./feeds/packages/utils/runc/Makefile
-# svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
+exit 0
+EOL
 
-# coolsnowwolf default software package replaced with Lienol related software package
-# rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
-# svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
+cat <<EOL >> package/base-files/files/etc/uci-defaults/99-MzWrt-settings
+#!/bin/sh
 
-# Add third-party software packages (The entire repository)
-# git clone https://github.com/libremesh/lime-packages.git package/lime-packages
-# Add third-party software packages (Specify the package)
-# svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
-# Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
-# sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
+# 设置主机名
+uci set system.@system[0].hostname='MzWrt'
 
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
+# 修改默认IP
+uci set network.lan.ipaddr='192.168.10.1'
 
+# 设置 2.4GHz 频段的国家为中国
+uci set wireless.radio0.country='CN'
+# 设置 5GHz 频段的国家为中国
+uci set wireless.radio1.country='CN'
+
+# 信道设置
+uci set wireless.radio0.channel='auto'  # 设置 2.4GHz 信道为 13
+uci set wireless.radio1.channel='auto'  # 设置 5GHz 信道为 36
+
+# 设置 2.4GHz 频段的无线模式（支持 802.11n，20MHz 或 40MHz，最大带宽 40MHz）
+uci set wireless.radio0.htmode='HE40'  # 设置 2.4GHz 频段为 40MHz 带宽
+
+# 设置 5GHz 频段的无线模式为 HE160（最大带宽 160MHz）
+uci set wireless.radio1.htmode='HE160'  # 设置 5GHz 带宽为 160MHz
+
+# 开启 Beamforming 功能
+uci set wireless.radio0.beamforming='1'  # 开启 2.4GHz 的 Beamforming
+uci set wireless.radio1.beamforming='1'  # 开启 5GHz 的 Beamforming
+
+# 开启 MU-MIMO 支持
+uci set wireless.radio0.mu_mimo='1'  # 开启 2.4GHz 的 MU-MIMO
+uci set wireless.radio1.mu_mimo='1'  # 开启 5GHz 的 MU-MIMO
+
+# 开启 OFDMA 支持
+uci set wireless.radio0.ofdma='1'    # 开启 2.4GHz 的 OFDMA
+uci set wireless.radio1.ofdma='1'    # 开启 5GHz 的 OFDMA
+
+# 设置 2.4GHz SSID 名称为 MzWrt-2.4G
+uci set wireless.default_radio0.ssid='MzWrt-2.4G'
+
+# 设置 5GHz SSID 名称为 MzWrt-5G
+uci set wireless.default_radio1.ssid='MzWrt-5G'
+
+# 提交无线配置
+uci commit wireless
+
+# 重启网络
+# /etc/init.d/network restart
+
+# 提交修改的默认IP
+uci commit network
+
+# 如果您确实需要重启系统或修改主机名，也可以执行
+# 提交主机名配置
+uci commit system
+# 重启系统
+# /etc/init.d/system restart
+
+EOL
+
+
+# 从 /etc/sysctl.d/ 目录中删除net.ipv4.tcp_fin_timeout=和net.ipv4.tcp_keepalive_time=因为下面已经定义了这两个的值防止被覆盖
+for conf_file in package/base-files/files/etc/sysctl.d/*.conf; do
+    if grep -q "net.ipv4.tcp_fin_timeout=" "$conf_file"; then
+        sed -i '/net.ipv4.tcp_fin_timeout=/s/^/#/' "$conf_file"
+        echo "Commented out net.ipv4.tcp_fin_timeout=30 in $conf_file"
+    fi
+
+    if grep -q "net.ipv4.tcp_keepalive_time=" "$conf_file"; then
+        sed -i '/net.ipv4.tcp_keepalive_time=/s/^/#/' "$conf_file"
+        echo "Commented out net.ipv4.tcp_keepalive_time=120 in $conf_file"
+    fi
+done
+
+# 设置sysctl.conf参数优化系统和网络
+cat <<EOL >> package/base-files/files/etc/sysctl.conf
+vm.swappiness=10
+vm.vfs_cache_pressure=50
+
+fs.nr_open=1200000
+fs.file-max=200000
+
+# Enable TCP SYN cookies
+net.ipv4.tcp_syncookies=1
+
+# Increase the maximum number of connections in the backlog
+net.core.somaxconn=65535
+
+# Increase the maximum number of queued packets
+net.core.netdev_max_backlog=1000
+
+# Increase buffer sizes for TCP
+net.core.rmem_default=65536
+net.core.wmem_default=65536
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+
+# TCP settings
+net.ipv4.tcp_max_syn_backlog=4096
+net.ipv4.tcp_synack_retries=1
+net.ipv4.tcp_keepalive_time=300
+net.ipv4.tcp_keepalive_intvl=15
+net.ipv4.tcp_keepalive_probes=5
+net.ipv4.tcp_fin_timeout=10
+net.ipv4.tcp_max_orphans=65536
+net.ipv4.tcp_mem=50576 64768 98152
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_wmem=4096 65536 16777216
+net.ipv4.tcp_orphan_retries=0
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_rfc1337=1
+
+EOL
